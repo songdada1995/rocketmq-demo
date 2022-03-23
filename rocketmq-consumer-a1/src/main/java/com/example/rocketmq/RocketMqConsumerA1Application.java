3227371817af2aad7b201968f6a3cb6acde82e99
songbo <songbo@aukeys.com>
2022-03-23 09:42:08 +0800

    简单rocketmq生产者及消费者代码

--- rocketmq-consumer-a1/src/main/java/com/example/rocketmq/RocketMqConsumerA1Application.java
+++ rocketmq-consumer-a1/src/main/java/com/example/rocketmq/RocketMqConsumerA1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RocketMqConsumerB1Application {
+public class RocketMqConsumerA1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(RocketMqConsumerB1Application.class, args);
+        SpringApplication.run(RocketMqConsumerA1Application.class, args);
     }
 
 }
