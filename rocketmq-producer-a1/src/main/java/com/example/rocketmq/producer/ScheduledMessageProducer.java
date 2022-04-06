5bb3c517623dcbc69c8927f136444cdffc3621a0
songbo <1152745700@qq.com>
2022-04-06 23:08:40 +0800

    增加延时消息生产和消费

---
+++ rocketmq-producer-a1/src/main/java/com/example/rocketmq/producer/ScheduledMessageProducer.java
@@ -0,0 +1,32 @@
+package com.example.rocketmq.producer;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+
+/**
+ * 发送延时消息
+ */
+public class ScheduledMessageProducer {
+
+    public static void main(String[] args) throws Exception {
+        // 实例化一个生产者来产生延时消息
+        DefaultMQProducer producer = new DefaultMQProducer("simple_scheduled_group");
+        producer.setNamesrvAddr("127.0.0.1:9876");
+        // 启动生产者
+        producer.start();
+        int totalMessagesToSend = 10;
+        for (int i = 0; i < totalMessagesToSend; i++) {
+            Message message = new Message("ScheduledTestTopic", ("Hello scheduled message " + i).getBytes());
+            // 设置延时等级3,这个消息将在10s之后发送(现在只支持固定的几个时间,详看delayTimeLevel)
+            // org/apache/rocketmq/store/config/MessageStoreConfig.java
+            // private String messageDelayLevel = "1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h";
+            // 现在RocketMq并不支持任意时间的延时，需要设置几个固定的延时等级，从1s到2h分别对应着等级1到18，消息消费失败会进入延时消息队列，消息发送时间与设置的延时等级和重试次数有关，详见代码SendMessageProcessor.java
+            message.setDelayTimeLevel(3);
+            // 发送消息
+            producer.send(message);
+        }
+        // 关闭生产者
+        producer.shutdown();
+    }
+
+}
